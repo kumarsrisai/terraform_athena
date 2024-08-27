31fb6d3867b4a7dbaa13d3c5e468f249d57a9cb9
Kumar Sri Sai Kottagundu <kumarsrisai08@gmail.com>
2024-08-27 18:04:45 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+
+resource "aws_s3_bucket" "athena_bucket" {
+  bucket = "terra-athena-bucket" 
+  acl    = "private"  
+}
+
+resource "aws_s3_object" "data_folder" {
+  bucket = aws_s3_bucket.athena_bucket.bucket
+  key    = "athena/" 
+}
+
+
+resource "aws_glue_catalog_table" "athena_table" {
+  name          = "terra_athena_table"
+  database_name = "terra_athena_database"
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.athena_bucket.bucket}/${aws_s3_object.data_folder.key}"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    serde_info {
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ","
+      }
+    }
+
+    columns {
+      name = "id"
+      type = "int"
+    }
+
+    columns {
+      name = "name"
+      type = "string"
+    }
+
+    columns {
+      name = "email"
+      type = "string"
+    }
+
+    columns {
+      name = "created_at"
+      type = "timestamp"
+    }
+
+    columns {
+      name = "status"
+      type = "boolean"
+    }
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "athena_table_name" {
+  value = aws_glue_catalog_table.athena_table.name
+}
