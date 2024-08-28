bd1191166bc40f5838f85a86c7a302077bde0652
Kumar Sri Sai Kottagundu <66506725+kumarsrisai@users.noreply.github.com>
2024-08-28 11:12:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,29 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
 }
 
-
-
+variable "s3_bucket_name" {
+  type        = string
+  default     = "terra-athena-bucket"
+}
 
 variable "s3_data_prefix" {
-  type = string
-  default = "result/"
-  
-}
-variable "glue_catalog_db" {
-  type = string
-  default = "terra_athena_database"
+  type        = string
+  default     = "data/"
 }
 
-variable "glue_catalog_table" {
-  type = string
-  default = "terra_athena_table"
+variable "athena_database_name" {
+  type        = string
+  default     = "terra_athena_database"
+}
+
+variable "athena_table_name" {
+  type        = string
+  default     = "terra_athena_table"
 }
 
 variable "athena_query" {
-  type = string
-  default = "SELECT * from terra_anthena_table LIMIT 10;"
-  
+  type        = string
+  default     = "SELECT * FROM terra_athena_table LIMIT 10;"
 }
